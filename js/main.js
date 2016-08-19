88cfa0d73662c4145423157bbd6c1f3a888c62fa
Sinnerion <a.dergachow@gmail.com>
2016-08-19 16:32:25 +0300

    Portfolio block finished

--- js/main.js
+++ js/main.js
@@ -13,11 +13,35 @@ $(document).ready(function(){
    });
 
    // Smooth transition to sections ----------------------------------------------
-    $("a.scrollto").click(function () {
+    $('a.scrollto').click(function () {
         elementClick = $(this).attr("href");
         destination = $(elementClick).offset().top;
         $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
         return false;
     });
 
+    // Portfolio preview's hover
+    $('.hover-hidden').hover(function() {
+        $(this).removeClass('hover-hidden');
+        }, function() {
+            $('.portfolio_preview-hover').addClass('hover-hidden');
+        }
+    );
+
+    // Portfolio filter
+    $('#filter').on('click', 'button', function () {
+        $('.active-button').removeClass('active-button');
+        $(this).addClass('active-button');
+        var buttonAttr = $(this).attr('data-filter');
+        $('.portfolio_preview').each(function(){
+            var blockAttr = $(this).attr('data-filter');
+            if( blockAttr !== buttonAttr && buttonAttr !== 'all' ) {
+                $(this).addClass('preview-hidden');
+            } else if( blockAttr === buttonAttr) {
+                $(this).removeClass('preview-hidden');
+            } else {
+                $('.portfolio_preview').removeClass('preview-hidden');
+            }
+        });
+    });
 });
